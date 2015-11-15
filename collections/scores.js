74701463561d6eca7a8944fb820bf146ec647150
chillee <horacehe2007@yahoo.com>
2015-11-15 12:34:49 -0800

    Set up the basic structure

---
+++ collections/scores.js
@@ -0,0 +1,43 @@
+Scores = new Mongo.Collection('scores');
+
+Scores.allow({
+	insert: function(userId, doc){
+		return !!userId;
+	}
+})
+
+Score = new SimpleSchema({
+	examName: {
+		type: String
+	},
+	userScore: {
+		type: double
+	},
+	maxScore: {
+		type: double
+	}
+});
+
+ScoresSchema = new SimpleSchema({
+	name: {
+		type: String,
+		label: "Name"
+	},
+	scores: {
+		type: [Score]
+	},
+	school: {
+		type: String,
+		label: "School"
+	},
+	createdAt: {
+		type: Date,
+		label: "Created At",
+		autoValue: function(){
+			return new Date();
+		},
+		autoform: {
+			type: "hidden";
+		}
+	}
+});
