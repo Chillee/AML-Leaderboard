3ad0e46935f8c9fe35f493f95335ed6988ec71f4
tonynater <tong.t.wang@gmail.com>
2015-12-02 21:05:05 -0800

    Added student rank and bootstrap table

--- client/leaderboard/Leaderboard.js
+++ client/leaderboard/Leaderboard.js
@@ -3,9 +3,28 @@ Meteor.startup(function() {
   Session.set("sort_by", {sumEarnedScore: -1, lastName: 1, firstName: 1});
 });
 
+function getRankList() {
+  var rankList = {}, prvScore = -1, idx = 1;
+  Students.find({}, {sort: {sumEarnedScore: -1}}).forEach(function(student) {
+    if (prvScore != student.sumEarnedScore) {
+      prvScore = student.sumEarnedScore;
+      rankList[student.sumEarnedScore.toString()] = idx;
+    }
+    ++idx;
+  });
+  return rankList;
+}
+
 Template.leaderboard.helpers({
-	students: function() {
-    return Students.find({}, {sort: Session.get("sort_by")});
+  students: function() {
+    var rankList = getRankList();
+    return Students.find({}, {
+      sort: Session.get("sort_by"),
+      transform: function(student) {
+        student.rank = rankList[student.sumEarnedScore.toString()];
+        return student;
+      }
+    });
 	}
 });
 
@@ -18,5 +37,9 @@ Template.sortings.events({
   },
   'click .sortLastName': function() {
     Session.set("sort_by", {lastName: 1, firstName: 1, sumEarnedScore: -1});
+  },
+  'click .sortGrade': function() {
+    Session.set("sort_by", {
+      grade: 1, sumEarnedScore: -1, lastName: 1, firstName: 1});
   }
 });
